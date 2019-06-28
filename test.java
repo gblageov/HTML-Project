7c390b8aae201f908cc527e4a9979a8f9d903100
gblagoev <gblagoev@htmlbox.net>
2019-06-28 15:25:12 +0300

    change 76

---
+++ test.java
@@ -0,0 +1,12 @@
+/**
+ * test
+ */
+public class test {
+
+    public static void main(String[] args) {
+        // String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String txt = "Please locate where 'locate' occurs!";
+        System.out.println(txt.indexOf("l"));
+        System.out.println("The lenght id the text is: " + txt.length());
+    }
+}
